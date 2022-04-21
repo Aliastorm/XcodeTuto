365f196d9986fb4b462e22de0d04910f74e002e7
jpons <jean-francois.pons@neotys.com>
2022-04-21 14:46:33 +0200

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Jean-françois Pons on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
